c249d7b015a45a93dae875ee38a3b19b6b627adf
Jaythan <jaythan@JaythandeMacBook-Pro.local>
2023-11-06 16:26:36 +0800

    fix: phone and layout

--- src/components/Register/index.tsx
+++ src/components/Register/index.tsx
@@ -3,65 +3,41 @@ import { useState } from "react";
 import "./index.css";
 import FormInput from "../FormInput";
 import { restaurantApi } from "../../api/api";
-
-interface IValue {
-  username: string;
-  email: string;
-  birthday: string;
-  password: string;
-  confirmPassword: string;
-  gender: string;
-}
+import PhoneInput from "../PhoneInput";
 
 const map: Record<string, any> = {
+  firstName: "",
+  lastName: "",
   username: "",
   phone: "",
   birthday: "",
   password: "",
   confirmPassword: "",
   gender: "",
+  region: "+86",
 };
+
+const focusMap: Array<boolean> = [];
 
 const Register = () => {
   const [values, setValues] = useState(map);
+  const [focus, setFocus] = useState(false);
+  const [isCheckTerms, setIsCheckTerms] = useState(false);
+
+  const handleCheck = (event: any) => {
+    setIsCheckTerms(event.target.checked);
+    console.log(event.target.checked);
+  };
 
   const inputs = [
-    {
-      id: 1,
-      name: "username",
-      type: "text",
-      placeholder: "請輸入用戶名",
-      errorMessage:
-        "Username should be 3-16 characters and shouldn't include any special character!",
-      label: "用戶名",
-      pattern: "^[A-Za-z0-9]{3,16}$",
-      required: true,
-    },
-    {
-      id: 2,
-      name: "phone",
-      type: "phone",
-      placeholder: "請輸入手機號",
-      errorMessage: "It should be a valid email address!",
-      label: "手機號碼",
-      required: true,
-    },
-    // {
-    //   id: 3,
-    //   name: "birthday",
-    //   type: "date",
-    //   placeholder: "請輸入生日",
-    //   label: "出生日期",
-    // },
     {
       id: 4,
       name: "password",
       type: "password",
       placeholder: "請輸入密碼",
-      errorMessage:
-        "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
-      label: "Password",
-      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+      errorMessage: "密碼六到二十位數字",
+      label: "密碼",
+      pattern: `^[0-9]{6,20}$`, //todo
       required: true,
     },
     {
@@ -69,8 +45,8 @@ const Register = () => {
       name: "confirmPassword",
       type: "password",
       placeholder: "請二次輸入密碼",
-      errorMessage: "Passwords don't match!",
-      label: "Confirm Password",
+      errorMessage: "密碼不匹配!",
+      // label: "Confirm Password",
       pattern: values.password,
       required: true,
     },
@@ -90,41 +66,93 @@ const Register = () => {
   const handleSubmit = (e: any) => {
     e.preventDefault();
     // getTableList(values);
+
     console.log(values);
   };
 
   const onChange = (e: any) => {
+    console.log(e.target.name, e.target.value);
     setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  // 獲取驗證碼
+  const requireCode = () => {
+    console.log(`${values.phone} 獲取驗證碼`);
   };
 
   return (
     <div className="form-wrapper">
       <form onSubmit={handleSubmit}>
-        <h1>歡迎</h1>
-        {inputs.map((input) => (
-          <FormInput key={input.id} {...input} value={values[input.name]} onChange={onChange} />
-        ))}
-        <div className="formInput">
-          <label>出生日期</label>
-          <input
-            type="text"
-            name="birthday"
-            className="form-control"
-            onFocus={(e) => (e.currentTarget.type = "date")}
-            onBlur={(e) => (e.currentTarget.type = "text")}
-            placeholder="请选择出生日期"
-          />
-        </div>
-        <div className="formInput">
-          <label>性別</label>
-          <div>
-            <input type="radio" name="gender" value="male" onChange={onChange} />
-            <label htmlFor="male">男性</label>
-            <input type="radio" name="gender" value="female" onChange={onChange} />
-            <label htmlFor="male">女性</label>
+        <h1>歡迎加入財神芸</h1>
+        <div className="form-content">
+          <div className="formInput">
+            {/* <label htmlFor="name" className="">
+            姓名
+          </label> */}
+            <div className="name-group">
+              <input
+                key={1}
+                onChange={onChange}
+                name="firstName"
+                className="name-group-sub"
+                placeholder="姓"
+              />
+              <input
+                key={2}
+                onChange={onChange}
+                name="lastName"
+                className="name-group-sub"
+                placeholder="名"
+              />
+            </div>
+            <span>{"需要輸入姓"}</span>
+          </div>
+
+          <div className="form-inline">
+            <div className="gender-input">
+              <label>性別</label>
+              <div className="gender-group">
+                <input type="radio" name="gender" value="male" onChange={onChange} />
+                <label htmlFor="male">男性</label>
+                <input type="radio" name="gender" value="female" onChange={onChange} />
+                <label htmlFor="male">女性</label>
+              </div>
+            </div>
+
+            <div className="birth-input">
+              <label>出生日期</label>
+              <input
+                type="text"
+                name="birthday"
+                className="form-control"
+                onChange={onChange}
+                onFocus={(e) => (e.currentTarget.type = "date")}
+                onBlur={(e) => (e.currentTarget.type = "text")}
+                placeholder="请选择出生日期"
+              />
+            </div>
+          </div>
+
+          {inputs.map((input) => (
+            <FormInput key={input.id} {...input} value={values[input.name]} onChange={onChange} />
+          ))}
+
+          <PhoneInput />
+
+          <div className="agree-checkbox">
+            <input
+              type="checkbox"
+              name="terms"
+              checked={isCheckTerms}
+              onChange={handleCheck}
+              required={true}
+            />
+            <p className="agree-checkbox-text">
+              本人已閱讀並同意<a href="">《隱私政策》</a>
+              <a href="">《用戶協議》</a>
+            </p>
           </div>
         </div>
-
         <button>立即加入</button>
       </form>
     </div>
